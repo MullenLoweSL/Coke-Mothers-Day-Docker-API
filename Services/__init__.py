e2e59eb818b0e028f1642a42be5eb75aa7d6b0a9
sohan dharmarajah <sdharmarajah@gmail.com>
2024-05-07 05:04:10 +0530

    Added test for ffmpegService

--- Services/__init__.py
+++ Services/__init__.py
@@ -1,3 +1,4 @@
 from .Singleton import Singleton
 from .SlackService import SlackService
 from .SunoService import SunoService
+from .ffmpegService import ffmpegService
--- az_http_testing/__init__.py
+++ az_http_testing/__init__.py
@@ -2,12 +2,20 @@ import logging
 import json
 import azure.functions as func
 from services.SunoService import SunoService
+from services.ffmpegService import ffmpegService
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     
-    prompt = "[Verse 1]\nයමන් බණ්ඩො වෙසක් බලන්ඩ\nගනින් බර බාගේ බඳින්ඩ\nපාර දිගේ එළි බල බල\nමහ පාරේ හරි කලබල‍\nයමන් බණ්ඩො වෙසක් බලන්ඩ\n\n[Verse 2]\nයමන් බණ්ඩො වෙසක් බලන්ඩ\nගනින් බර බාගේ බඳින්ඩ\nපාර දිගේ එළි බල බල\nමහ පාරේ හරි කලබල‍\nයමන් බණ්ඩො වෙසක් බලන්ඩ"
-    tag = "Rock, with electric guitar"
-    title = "Enjoy Life"
-    suno_song_id = SunoService().custom_generate(prompt, tag, title)
-    return func.HttpResponse(json.dumps({"id": suno_song_id}), status_code=200, mimetype="application/json")
+    # prompt = "[Verse 1]\nයමන් බණ්ඩො වෙසක් බලන්ඩ\nගනින් බර බාගේ බඳින්ඩ\nපාර දිගේ එළි බල බල\nමහ පාරේ හරි කලබල‍\nයමන් බණ්ඩො වෙසක් බලන්ඩ\n\n[Verse 2]\nයමන් බණ්ඩො වෙසක් බලන්ඩ\nගනින් බර බාගේ බඳින්ඩ\nපාර දිගේ එළි බල බල\nමහ පාරේ හරි කලබල‍\nයමන් බණ්ඩො වෙසක් බලන්ඩ"
+    # tag = "Rock, with electric guitar"
+    # title = "Enjoy Life"
+    # suno_song_id = SunoService().custom_generate(prompt, tag, title)
+    # return func.HttpResponse(json.dumps({"id": suno_song_id}), status_code=200, mimetype="application/json")
+
+
+    mp3_path = "/Users/sohan/Desktop/Coke/test/sample.mp3"
+    png_path = "/Users/sohan/Desktop/Coke/test/cover.png"
+    output_path = "/Users/sohan/Desktop/Coke/test/output.mp4"
+    result = ffmpegService().generate_mp4(mp3_path, png_path, output_path)
+    return func.HttpResponse(json.dumps({"output_path": output_path}), status_code=200, mimetype="application/json")
