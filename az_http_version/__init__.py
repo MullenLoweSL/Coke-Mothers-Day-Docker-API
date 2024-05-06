1fafbe52980534f55debcaecd811419f7bc3f39e
sohan dharmarajah <sdharmarajah@gmail.com>
2024-05-06 16:39:12 +0530

    Update tag

--- az_http_version/__init__.py
+++ az_http_version/__init__.py
@@ -7,5 +7,5 @@ import os
 # YAGNI for anything else...
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    version = "v-dev-0.02"
+    version = "v-dev-0.03"
     return func.HttpResponse(json.dumps({"version": version}), status_code=200, mimetype="application/json")    
