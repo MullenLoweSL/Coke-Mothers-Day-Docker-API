9de741074f867e0c47d732bbbefb31172db239c1
sohan dharmarajah <sdharmarajah@gmail.com>
2024-05-08 14:53:01 +0530

    Added share_url

--- az_http_result/__init__.py
+++ az_http_result/__init__.py
@@ -16,6 +16,7 @@ def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
             "image_url":"https://i.ibb.co/1JsmZQm/653-400x400.jpg",
             "audio_url":"https://mullenlowedemo.blob.core.windows.net/coke-mothers-day/test/song.mp3?sp=r&st=2024-04-30T15:13:38Z&se=2024-08-27T23:13:38Z&sv=2022-11-02&sr=b&sig=k4aquYkICPMmu8M0ZDgBipv2fS5xvRVCuSqmARm18H8%3D",
             "title":"DUMMY TITLE",
-            "description":"DUMMY DESCRIPTION"
+            "description":"DUMMY DESCRIPTION",
+            "share_url": "www.google.com"
         }
     return func.HttpResponse(json.dumps({"result": result}), status_code=200, mimetype="application/json")
