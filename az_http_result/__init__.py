e4e6b014a8e5e4dcf0c3baf71b42e118ec80a32f
sohan dharmarajah <sdharmarajah@gmail.com>
2024-05-08 14:51:44 +0530

    Added title and description

--- az_http_result/__init__.py
+++ az_http_result/__init__.py
@@ -14,6 +14,8 @@ def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     if random_boolean():
         result = {
             "image_url":"https://i.ibb.co/1JsmZQm/653-400x400.jpg",
-            "audio_url":"https://mullenlowedemo.blob.core.windows.net/coke-mothers-day/test/song.mp3?sp=r&st=2024-04-30T15:13:38Z&se=2024-08-27T23:13:38Z&sv=2022-11-02&sr=b&sig=k4aquYkICPMmu8M0ZDgBipv2fS5xvRVCuSqmARm18H8%3D"
+            "audio_url":"https://mullenlowedemo.blob.core.windows.net/coke-mothers-day/test/song.mp3?sp=r&st=2024-04-30T15:13:38Z&se=2024-08-27T23:13:38Z&sv=2022-11-02&sr=b&sig=k4aquYkICPMmu8M0ZDgBipv2fS5xvRVCuSqmARm18H8%3D",
+            "title":"DUMMY TITLE",
+            "description":"DUMMY DESCRIPTION"
         }
     return func.HttpResponse(json.dumps({"result": result}), status_code=200, mimetype="application/json")
