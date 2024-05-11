a44c70664579596da66e53289816d65e517a7d39
sohan dharmarajah <sdharmarajah@gmail.com>
2024-05-11 12:17:31 +0530

    Passing duration

--- az_http_result/__init__.py
+++ az_http_result/__init__.py
@@ -5,6 +5,32 @@ import azure.functions as func
 from controllers.SessionController import SessionController
 from services.BlobService import BlobService
 from services.SunoService import SunoService
+from mutagen.mp3 import MP3
+import tempfile
+import requests
+
+def get_audio_duration(url):
+    # Create a temporary file
+    temp_file = tempfile.NamedTemporaryFile(delete=False)
+
+    # Download the file
+    try:
+        response = requests.get(url, stream=True)
+        for chunk in response.iter_content(chunk_size=128):
+            temp_file.write(chunk)
+        temp_file.close()
+    except Exception as e:
+        logging.error(f"Failed to download file: {e}")
+        return 120
+
+    # Get the duration
+    try:
+        audio = MP3(temp_file.name)
+        length = audio.info.length
+        return length
+    except Exception as e:
+        logging.error(f"Failed to get audio duration: {e}")
+        return 120
 
 def random_boolean():
     return random.random() < 0.5
@@ -21,9 +47,11 @@ def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
         # get song URL from Suno
         suno_song_id = handler.get_suno_song_id(session_id)
         song_url = SunoService().get_song_URL(suno_song_id)
+        duration = get_audio_duration(song_url)
         result = {
             "image_url": BlobService().get_url_from_blob_path(session_id + "/image_branded.png", expiration_minutes=5),
             "audio_url": song_url,
+            "duration": duration,
             "title":"DUMMY TITLE",
             "description":"DUMMY DESCRIPTION"
         }
