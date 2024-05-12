a7e3f2bf445282f6ce59ba23dcc33273282698be
sohan dharmarajah <sdharmarajah@gmail.com>
2024-05-12 12:19:30 +0530

    Sleep between Suno retries

--- services/SunoService.py
+++ services/SunoService.py
@@ -1,7 +1,9 @@
 import os
 import json
 import requests
+import time
 from .Singleton import Singleton
+
 
 class SunoService(metaclass=Singleton):
 
@@ -9,6 +11,19 @@ class SunoService(metaclass=Singleton):
         self.custom_generate_url = os.environ["SUNO_API"] + "/custom_generate"
         self.ai_generate_url = os.environ["SUNO_API"] + "/generate"
         self.get_info = os.environ["SUNO_API"] + "/get?ids="
+
+    # def custom_generate(self, prompt, tag, title):
+    #     headers = {'Content-Type': 'application/json'}
+    #     payload = {
+    #         "prompt": prompt,
+    #         "tags": tag,
+    #         "title": title,
+    #         "make_instrumental": False
+    #     }
+    #     response = requests.request("POST", self.custom_generate_url, headers=headers, data=json.dumps(payload))
+    #     response_json = response.json()
+    #     # response JSON always has 2 entries, take 1st one (arbitrarily)
+    #     return response_json[0]['id']
 
     def custom_generate(self, prompt, tag, title):
         headers = {'Content-Type': 'application/json'}
@@ -18,10 +33,19 @@ class SunoService(metaclass=Singleton):
             "title": title,
             "make_instrumental": False
         }
-        response = requests.request("POST", self.custom_generate_url, headers=headers, data=json.dumps(payload))
-        response_json = response.json()
-        # response JSON always has 2 entries, take 1st one (arbitrarily)
-        return response_json[0]['id']
+
+        max_retries = 3
+        for _ in range(max_retries):
+            try:
+                response = requests.request("POST", self.custom_generate_url, headers=headers, data=json.dumps(payload))
+                response_json = response.json()
+                # response JSON always has 2 entries, take 1st one (arbitrarily)
+                return response_json[0]['id']
+            except (IndexError, KeyError, ValueError):
+                time.sleep(2)  # Sleep for 2 seconds before retrying
+                continue  # Retry the request
+
+        raise Exception("Failed to get valid response after {} retries".format(max_retries))
 
     def ai_generate(self, prompt):
         headers = {'Content-Type': 'application/json'}
